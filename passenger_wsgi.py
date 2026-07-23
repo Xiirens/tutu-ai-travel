d6701dd35a46d8223f30ff19fbb7c913f460e508
Xiirens <Xiirens@users.noreply.github.com>
2026-07-23 18:43:19 +0300

    Fix Passenger virtualenv detection

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -4,9 +4,10 @@ from pathlib import Path
 
 
 PROJECT_DIR = Path(__file__).resolve().parent
+VENV_DIR = Path("/var/www/u3586612/data/tutu_env")
 VENV_PYTHON = Path("/var/www/u3586612/data/tutu_env/bin/python")
 
-if VENV_PYTHON.exists() and Path(sys.executable).resolve() != VENV_PYTHON.resolve():
+if VENV_PYTHON.exists() and Path(sys.prefix).resolve() != VENV_DIR.resolve():
     os.execl(str(VENV_PYTHON), str(VENV_PYTHON), *sys.argv)
 
 sys.path.insert(0, str(PROJECT_DIR))
